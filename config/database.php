d262f3f63db43eb7a11b20c6ea84636cbb422a93
SamarAli <62724986+Samar-Ali666@users.noreply.github.com>
2024-05-01 13:12:19 +0500

    Update database.php
    mysql ssl cert added

--- config/database.php
+++ config/database.php
@@ -51,6 +51,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'MYSQL_ATTR_SSL_CA' => env('MYSQL_ATTR_SSL_CA', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
